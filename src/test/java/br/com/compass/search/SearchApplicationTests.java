ed5c7934750f7b02b09827de8ef396091f29813b
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-09-06 21:40:30 -0300

    para testes

--- src/test/java/br/com/compass/search/SearchApplicationTests.java
+++ src/test/java/br/com/compass/search/SearchApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.compass.search;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SearchApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.compass.search;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SearchApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
